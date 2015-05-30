4cf03f5640eff55358106b6eb4841f8df36a314c
NitinMahajan1 <nitin_mahajan@icloud.com>
2015-05-30 17:03:42 +0100

    v 0.1
    Validated input

--- best.R
+++ best.R
@@ -1,3 +1,14 @@
+setwd("C:/Users/nitin/OneDrive/WIP/Coursera/Programming-Assignment-3")
 
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-head(outcome)
+best <- function(state, outcome) {
+  ## Read outcome data
+  outcomes <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+ 
+  ## Check that state and outcome are valid
+  if ((count(subset(outcomes,outcomes$State==state))) < 1)   stop("invalid state")
+  if (!(outcome=="heart attack" | outcome=="heart failure" | outcome=="pneumonia"))   stop("invalid outcome")
+  
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate  
+} 
+
